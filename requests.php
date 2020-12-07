df751e2701018840776ed5a4bc417614b4ebb90a
Haroldo Lopez <haroldolopeziron@gmail.com>
2020-12-07 17:20:32 -0600

    Agregando comentarios

--- requests.php
+++ requests.php
@@ -1,7 +1,9 @@
 <?php
 
+    // Arreglo donde se guardará la respuesta final
     $response = array();
 
+    // Función que evalua qué respuesta HTML deberá generar
     function evaluarFormulario($jsonTmp,&$formFinal) {
         $formFinal .= "\t<div class='formGroup'>\n";
         if($jsonTmp["tipo"] == "text"){
@@ -36,23 +38,32 @@
     }
 
 
+    // Se recibe desde la variable $_POST en la posición opcion qué acción debe tomar
     if(isset($_POST["opcion"])){
 
+        // Variable que almacena lo que ingresó el usuario
         $texto = $_POST["texto"];
 
+        // Se utiliza la estructura switch para evaluar la opción
         switch($_POST["opcion"]){
+
+            // El caso de generación de tabla desde un excel
             case "tablaExcel":
 
+                // Se utiliza la variable temporal para guardar el string de HTML que retornará
                 $tablaFinal = "<table class='tabla' border=1>\n";
 
                 if($texto != ""){ 
 
+                    // Se separa la cadena recibida por bajados de línea para obtener las filas
                     $rowTmp = explode("\n", $texto);
 
                     foreach($rowTmp as $indexRows => $rows){
+                        // Se separa la cadena recibida por tabulaciones para obtener las columnas
                         $colTmp = explode("\t", $rows);
                         $tablaFinal .= "\t<tr>\n\t\t";
                         foreach($colTmp as $indexCols => $cols){
+                            // Se eliminan los espacios en blanco que se generan
                             $valorTmp = rtrim($cols);
                             $tablaFinal .= "<td>$valorTmp</td>\n\t\t";
                         }
@@ -62,11 +73,13 @@
 
                     $tablaFinal .= "</table>";
 
+                    // Se guarda en la variable $response un arreglo con el status y la información a mostrar
                     $response = array(
                         "status" => "ok",
                         "data" => $tablaFinal
                     );
 
+                    // Se utiliza esta función para convertir de arreglo de php a un string tipo JSON
                     echo json_encode($response);
 
                 } else {
@@ -75,13 +88,17 @@
 
             break;
 
+            // El caso de generación de formularios HTML
             case 'formularioJSON':
                 
+                // Se transforma el string a un arreglo de php
                 $jsonTmp = json_decode($texto,true);
 
+                // En esta variable se almancenará lo que se mostrará en HTML
                 $formFinal = "<form class='formGen' action='' method=''>\n\n";
 
                 for($i = 0; $i< count($jsonTmp); $i++){
+                    // Se utiliza la función para evaluar qué control de formulario deberá escribir en la variable
                     evaluarFormulario($jsonTmp[$i],$formFinal);
                 }
 
